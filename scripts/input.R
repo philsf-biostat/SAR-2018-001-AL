452c1a4a1f1c8e8e5fdac089b2c2571b8f51e6cf
Felipe Figueiredo <philsf79@gmail.com>
2018-01-26 05:55:17 -0200

    colnames simplificados

--- scripts/input.R
+++ scripts/input.R
@@ -3,3 +3,6 @@ library(data.table)
 
 dados <- read_excel("dataset/resultados trabalho SBCJ.xlsx")
 dados <- as.data.table(dados)
+
+# simplificar colnames
+names(dados)[5:7] <- c("loc", "da", "lca")
